5e3afb80cfcfb03994e1930d8d973be554e6d0ff
oneway101 <oneway.first@gmail.com>
2020-04-20 15:06:56 -0600

    about us css

---
+++ src/components/About.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import MeshPants from '../assets/mesh-pants.png'
+import MeshShirt from '../assets/mesh-shirt.png'
+
+function About() {
+
+  const data = [
+    {
+      action: "Are",
+      content: "Variant is a fashion-tech incubator that utilizes 3D knitting technology and body-scanning to create customizable knitwear. Our software provides brands with a cost-effective platform to co-create products with their customers. Our in-house line, Calamigos Malibu, showcases how Variant products can be integrated into existing lifestyle brands and customized for your business’ needs."
+    },
+    {
+      action: "Ideate",
+      content: "The idea for Variant was hatched in Malibu, California, a community filled with creatives and founders who respect the environment and seek sustainable solutions. Here, we nurture the best design concepts within technology and apparel. With access to leading factories in Los Angeles and top yarn suppliers and material innovators in Asia, Variant is uniquely positioned to source globally and produce locally. Our goal is to revolutionize current manufacturing processes and rally teams to change their technology infrastructure and mindset."
+    },
+    {
+      action: "Create",
+      content: "Our patented Customizer uses 3D modeling to show you exactly how your garment will look and fit on your body. 3D knitting produces custom, made to measure, three-dimensional garments directly from the machine, so each piece requires less sewing and has more comfort and durability. Our knitwear technicians can inlay smart fibers, fabrics and even sensors into each pattern; breathability and ventilation can be built in according to your measurements. Our process is ideal for apparel, and fashion and home accessories."
+    },
+    {
+      action: "Believe",
+      content: "We believe that conscious manufacturing makes a difference, and we’re committed to reducing fashion waste with our on-demand manufacturing platform. That means garments are only created when you order them, eliminating excess inventory.  We also use sustainably-sourced and recycled fibers as well as recyclable smart fabrics. We care about our planet and preserving our natural resources for future generations."
+    }
+  ]
+  return (
+    <div id="about">
+      <div className="banner-wrapper">
+        <div className="sliding-banner about"></div>
+      </div>
+      {
+        data.map((data, idx) => {
+          return (
+            <div className="content-wrapper" key={idx}>
+              <div className="mesh">
+                <img src={ idx % 2 === 0 ? MeshPants : MeshShirt} alt="mesh-img" />
+              </div>
+              <div className="sticky-content">
+                <span>We</span>
+                <span className="action">{data.action}</span>
+                <p>{data.content}</p>
+              </div>
+            </div>
+          )
+        })
+      }
+
+    </div>
+  )
+}
+
+export default About
