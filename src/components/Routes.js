afce8f0e7a8a1f34547c087b0685907091fd86ef
oneway101 <oneway.first@gmail.com>
2020-04-19 21:23:43 -0600

    responsive partners page

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -6,11 +6,13 @@ import Post from './Post'
 import Contact from './Contact'
 import Terms from './Terms'
 import Privacy from './Privacy'
+import Partners from './Partners'
 
 function Routes() {
   return (
     <Switch>
       <Route exact path="/" component={Home} />
+      <Route exact path="/partners" component={Partners} />
       <Route exact path="/contact" component={Contact} />
       <Route exact path="/privacy-policy" component={Privacy} />
       <Route exact path="/terms-and-conditions" component={Terms} />
