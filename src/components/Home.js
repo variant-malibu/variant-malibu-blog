58c7d62c9269e89bd5ba3c99493ed7b17b68e331
oneway101 <oneway.first@gmail.com>
2020-04-20 20:30:20 -0600

    home page css

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,50 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import * as ScrollMagic from 'scrollmagic'
+import gsap from "gsap"
+import { ScrollMagicPluginGsap } from "scrollmagic-plugin-gsap";
+
 
 const Home = () => {
 
+  useEffect (() => {
+    sectionWipes()
+  },[])
+
+  const sectionWipes = () => {
+    let controller = new ScrollMagic.Controller({
+			globalSceneOptions: {
+				triggerHook: 'onLeave',
+				duration: "200%"
+			}
+		});
+
+		// get all slides
+		var slides = document.querySelectorAll("section.panel");
+
+		// create scene for every slide
+		for (var i=0; i<slides.length; i++) {
+			new ScrollMagic.Scene({
+					triggerElement: slides[i]
+				})
+				.setPin(slides[i], {pushFollowers: false})
+				.addTo(controller);
+		}
+  }
+
   return (
     <div id="home">
-      <h1>Custom 3D Knitwear</h1>
-      <h1>Partners</h1>
-      <h1>About Us</h1>
-      <h1>Fashion x Tech</h1>
-      <h1>Contact</h1>
+      <section className="panel overview">
+        <h1>Custom 3D Knitwear</h1>
+      </section>
+      <section className="panel partners">
+        <h1>Partners</h1>
+      </section>
+      <section className="panel blog">
+        <h1>Fashion<br/>x<br/>Tech</h1>
+      </section>
+      <section className="panel contact">
+        <h1>Contact</h1>
+      </section>
     </div>
   )
 }
