a133c9ec49c0722a10a7758cf4dd35b9e3e7ad0d
oneway101 <oneway.first@gmail.com>
2020-05-05 20:25:30 -0600

    removed scrolling animation

--- src/components/About.js
+++ src/components/About.js
@@ -6,32 +6,12 @@ import Section from './Section'
 
 function About() {
 
-  const observerRoot = useRef(null)
-
-  const handleScroll = () => {
-    let scrollEndPosition = document.body.clientHeight - window.innerHeight - 250
-    if (window.scrollY > 300 && window.scrollY <= scrollEndPosition) {
-      fadeIn(observerRoot.current)
-    } else if (window.scrollY <= 300 || window.scrollY > scrollEndPosition) {
-      fadeOut(observerRoot.current)
-    }
-  }
-
-  useEffect(()=> {
-    observerRoot.current.style.opacity = 0
-    window.addEventListener('scroll', handleScroll);
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    }
-  },[])
-
-
   return (
     <div id="about">
       <div className="banner-wrapper">
         <div className="sliding-banner about"></div>
       </div>
-      <div id="observerRoot" ref={observerRoot}>
+      <div className="sections">
       {
         data.map((data, idx) => {
           return <Section data={data} idx={idx} key={idx} />
--- src/components/Section.js
+++ src/components/Section.js
@@ -6,46 +6,17 @@ import {fadeIn, fadeOut, clipIn, clipOut} from '../helpers/gsap'
 
 function Section(props) {
   const { data, idx } = props
-  const section = useRef(null)
-  const we = useRef(null)
-  const mask = useRef(null)
-
-  useEffect(()=> {
-    const observer = new IntersectionObserver(([entry]) => {
-        if (entry.isIntersecting) {
-          fadeIn(entry.target.children[0])
-          clipIn(entry.target.children[2])
-        } else {
-          fadeOut(entry.target.children[0])
-          clipOut(entry.target.children[2])
-        }
-    },{
-      root: null,
-      rootMargin: "70% 0% 0% 0%",
-      threshold: 0.75
-    })
-
-    console.log(section)
-    if (section.current) {
-      observer.observe(section.current)
-    }
-
-    return () => {
-      if (section) {
-        observer.unobserve(section.current)
-      }
-    }
-  },[])
 
   return (
-    <section className={"content-wrapper " + data.action.toLowerCase()} ref={section}>
+    <section className={"content-wrapper " + data.action.toLowerCase()}>
       <div className="mesh">
         <img src={ idx % 2 === 0 ? pants : shirt} alt="mesh-img"/>
       </div>
-        <span className="we" ref={we}>We</span>
-        <span className="action">{data.action}</span>
-      <div className="mask" ref={mask}></div>
       <div className="content">
+        <div>
+          <span className="we">We</span>
+          <span className="action">{data.action}</span>
+        </div>
         <p>{data.content}</p>
       </div>
     </section>
