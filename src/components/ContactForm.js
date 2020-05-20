c3e534dee160b56f1587bc33c3dbfa5e0144fa0d
oneway101 <oneway.first@gmail.com>
2020-05-20 15:53:25 -0600

    footer newsletter input fix

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -28,7 +28,6 @@ const ContactForm = () => {
 
   const handleChange = (evt) => {
     setAlert("")
-    console.log("ENV Variables:", process.env.REACT_APP_BACKEND)
     setInputs({...inputs, [evt.target.name] : evt.target.value })
   }
 
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,31 +1,32 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-import {Grid} from '@material-ui/core'
 import {ReactComponent as InstagramIcon} from '../assets/instagram-icon.svg'
 import NewsletterForm from './NewsletterForm'
 
 function Footer() {
   return (
-    <Grid id="footer" container justify="space-between" >
-      <Grid className="section" item xs={12} sm={4}>
-        <h5 className="title">QUICK LINK</h5>
-        <li><Link to="/privacy-policy">PRIVACY POLICY</Link></li>
-        <li><Link to="/terms-and-conditions">TERMS & CONDITIONS</Link></li>
-        <li><Link to="/contact">CONTACT</Link></li>
-        <li><Link to="/faqs">FAQs</Link></li>
-      </Grid>
-      <Grid className="section" item xs={12} sm={4}>
-        <h5 className="title">SOCIAL MEDIA</h5>
-        <a href="https://www.instagram.com/variantmalibu/"><InstagramIcon /></a>
-      </Grid>
-      <Grid className="section" item xs={12} sm={4}>
-        <h5 className="title">NEWSLETTER</h5>
-        <NewsletterForm />
-      </Grid>
-      <Grid item xs={12} sm={12}>
-        <p className="copyright">Copyright &copy; 2020 Variant Malibu</p>
-      </Grid>
-    </Grid>
+    <div id="footer">
+      <div className="section-wrapper">
+        <div className="section">
+          <h5 className="title">QUICK LINK</h5>
+          <li><Link to="/privacy-policy">PRIVACY POLICY</Link></li>
+          <li><Link to="/terms-and-conditions">TERMS & CONDITIONS</Link></li>
+          <li><Link to="/contact">CONTACT</Link></li>
+          <li><Link to="/faqs">FAQs</Link></li>
+        </div>
+        <div className="section">
+          <h5 className="title">SOCIAL MEDIA</h5>
+          <a href="https://www.instagram.com/variantmalibu/"><InstagramIcon /></a>
+        </div>
+        <div className="section">
+          <h5 className="title">NEWSLETTER</h5>
+          <NewsletterForm />
+        </div>
+      </div>
+      <div className="copyright">
+        <p>Copyright &copy; 2020 Variant Malibu</p>
+      </div>
+    </div>
   )
 }
 
